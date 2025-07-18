0d0d1b8d41f9a8ebd4d07464f84d032e6dbb2079
leopimentel-luizalabs <leonardo.ferreira@luizalabs.com>
2025-07-18 11:52:43 +0000

    Update siteProps with new user information and add bounce animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,11 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "Leozim",
+  title: "Software Engineer",
+  email: "email@gmail.com",
+  gitHub: "leo",
+  instagram: "insta",
   linkedIn: "satyanadella",
   medium: "",
   twitter: "microsoft",
